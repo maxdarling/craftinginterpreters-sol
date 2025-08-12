c1373e4c327415d8643c295b8daa57b5bda4ef08
Max Darling <maxdarling11@gmail.com>
2025-08-12 14:49:34 -0400

    fix indentation

--- java/com/craftinginterpreters/lox/Scanner.java
+++ java/com/craftinginterpreters/lox/Scanner.java
@@ -141,92 +141,93 @@ class Scanner {
     }
 
     private void identifier() {
-        while (isAlphaNumeric(peek()))
-            advance();
+      while (isAlphaNumeric(peek()))
+        advance();
 
-        String text = source.substring(start, current);
-        TokenType type = keywords.get(text);
-        if (type == null) type = IDENTIFIER;
-        addToken(type);
+      String text = source.substring(start, current);
+      TokenType type = keywords.get(text);
+      if (type == null)
+        type = IDENTIFIER;
+      addToken(type);
     }
 
     private void number() {
+      while (isDigit(peek()))
+        advance();
+      // Look for a fractional part.
+      if (peek() == '.' && isDigit(peekNext())) {
+        // Consume the "."
+        advance();
         while (isDigit(peek()))
-            advance();
-        // Look for a fractional part.
-        if (peek() == '.' && isDigit(peekNext())) {
-            // Consume the "."
-            advance();
-            while (isDigit(peek()))
-                advance();
-        }
-        addToken(NUMBER,
-                Double.parseDouble(source.substring(start, current)));
+          advance();
+      }
+      addToken(NUMBER,
+          Double.parseDouble(source.substring(start, current)));
     }
 
     private void string() {
-        while (peek() != '"' && !isAtEnd()) {
-            if (peek() == '\n')
-                line++;
-            advance();
-        }
-        if (isAtEnd()) {
-            Lox.error(line, "Unterminated string.");
-            return;
-        }
-        // The closing ".
+      while (peek() != '"' && !isAtEnd()) {
+        if (peek() == '\n')
+          line++;
         advance();
-        // Trim the surrounding quotes.
-        String value = source.substring(start + 1, current - 1);
-        addToken(STRING, value);
+      }
+      if (isAtEnd()) {
+        Lox.error(line, "Unterminated string.");
+        return;
+      }
+      // The closing ".
+      advance();
+      // Trim the surrounding quotes.
+      String value = source.substring(start + 1, current - 1);
+      addToken(STRING, value);
     }
 
     private boolean match(char expected) {
-        if (isAtEnd())
-            return false;
-        if (source.charAt(current) != expected)
-            return false;
-        current++;
-        return true;
+      if (isAtEnd())
+        return false;
+      if (source.charAt(current) != expected)
+        return false;
+      current++;
+      return true;
     }
 
     private char peek() {
-        if (isAtEnd())
-            return '\0';
-        return source.charAt(current);
+      if (isAtEnd())
+        return '\0';
+      return source.charAt(current);
     }
 
     private char peekNext() {
-        if (current + 1 >= source.length())
-            return '\0';
-        return source.charAt(current + 1);
+      if (current + 1 >= source.length())
+        return '\0';
+      return source.charAt(current + 1);
     }
 
     private boolean isAlpha(char c) {
-        return (c >= 'a' && c <= 'z') ||
-                (c >= 'A' && c <= 'Z') ||
-                c == '_';
+      return (c >= 'a' && c <= 'z') ||
+          (c >= 'A' && c <= 'Z') ||
+          c == '_';
     }
 
     private boolean isAlphaNumeric(char c) {
-        return isAlpha(c) || isDigit(c);
+      return isAlpha(c) || isDigit(c);
     }
 
     private boolean isDigit(char c) {
-        return c >= '0' && c <= '9';
+      return c >= '0' && c <= '9';
     }
 
     private char advance() {
-        current++;
-        return source.charAt(current - 1);
+      current++;
+      return source.charAt(current - 1);
     }
 
     private void addToken(TokenType type) {
-        addToken(type, null);
+      addToken(type, null);
     }
 
     private void addToken(TokenType type, Object literal) {
-        String text = source.substring(start, current);
-        tokens.add(new Token(type, text, literal, line));
+      String text = source.substring(start, current);
+      tokens.add(new Token(type, text, literal, line));
     }
 }
